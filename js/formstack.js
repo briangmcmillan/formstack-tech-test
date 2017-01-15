1fa9122a2fd1ab929fe65521b6443b7c04deadae
Brian McMillan <brianm_91@hotmail.com>
2017-01-15 18:43:48 -0500

    Committing formstack tech test

---
+++ js/formstack.js
@@ -0,0 +1,9 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('formstack', [])
+        .controller('HeaderController', function Header() {
+            this.title = 'Hello Brian!';
+        });
+})();
