493e884c789a22fb1fc917c175cd3e35b03c0e98
Brian McMillan <brianm_91@hotmail.com>
2017-01-15 19:19:05 -0500

    Updated invalid number function

--- js/formstack.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    'use strict';
-
-    angular
-        .module('formstack', [])
-        .controller('HeaderController', function Header() {
-            this.title = 'Hello Brian!';
-        });
-})();
